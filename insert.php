5a3373406608b287e602d458ef0e8d0f4418064b
Daniel Paredes <hparedes32>
2017-05-16 11:25:51 +0200

    agregue un comentario

--- insert.php
+++ insert.php
@@ -9,7 +9,7 @@ $link = mysqli_connect('localhost:3306', 'root', '34403853', 'lo53');
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
- 
+ // ejemplo para github
 // Attempt insert query execution
 $sql = "INSERT INTO map (description, px_width, px_height, m_width, m_height, content) VALUES ('segundo valor', 12, 12, 12, 12, 'coso')";
 if(mysqli_query($link, $sql)){
